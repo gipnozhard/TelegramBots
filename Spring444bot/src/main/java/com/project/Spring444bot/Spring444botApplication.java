ed3cbb1d6cd3dfd09f475ecfd3feecc205876cdd
gipnozhard <71705375+gipnozhard@users.noreply.github.com>
2022-12-06 00:23:29 +0300

    Add files via upload

---
+++ Spring444bot/src/main/java/com/project/Spring444bot/Spring444botApplication.java
@@ -0,0 +1,13 @@
+package com.project.Spring444bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring444botApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring444botApplication.class, args);
+	}
+
+}
